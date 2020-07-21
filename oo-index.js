38f118aeb995e94b6118b5c6f327e91c4f6d98be
Madison McCardle <madison.mccardle@powerhrg.com>
2020-07-21 15:25:47 -0400

    Done.

--- oo-index.js
+++ oo-index.js
@@ -14,34 +14,57 @@ class Unicorn {
     this.health = 1000
   }
 
+  restart() {
+    while (eventArea.hasChildNodes()) {
+        eventArea.removeChild(eventArea.lastChild);
+    }
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode("Out on the street, ready to go!"));
+    eventArea.prepend(div);
+    imageArea.className = 'new';
+  }
+
+  attack() {
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode("'ATTAAAAAAAAAK!''"));
+    eventArea.prepend(div);
+    imageArea.className = 'attack';
+  }
+
+  defend() {
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode("You put sunglasses on."));
+    eventArea.prepend(div);
+    imageArea.className = 'defend';
+  }
 }
 
 
-Unicorn.prototype.restart = function() {
-  while (eventArea.hasChildNodes()) {
-    eventArea.removeChild(eventArea.lastChild);
-}
-var div = document.createElement("div");
-div.appendChild(document.createTextNode("Out on the street, ready to go!"));
-eventArea.prepend(div);
-imageArea.className = 'new';
-}
+// Unicorn.prototype.restart = function() {
+//   while (eventArea.hasChildNodes()) {
+//     eventArea.removeChild(eventArea.lastChild);
+// }
+// var div = document.createElement("div");
+// div.appendChild(document.createTextNode("Out on the street, ready to go!"));
+// eventArea.prepend(div);
+// imageArea.className = 'new';
+// }
 
 
-Unicorn.prototype.attack = function() {
-  var div = document.createElement("div");
-  div.appendChild(document.createTextNode("'ATTAAAAAAAAAK!''"));
-  eventArea.prepend(div);
-  imageArea.className = 'attack';
-}
+// Unicorn.prototype.attack = function() {
+//   var div = document.createElement("div");
+//   div.appendChild(document.createTextNode("'ATTAAAAAAAAAK!''"));
+//   eventArea.prepend(div);
+//   imageArea.className = 'attack';
+// }
 
 
-Unicorn.prototype.defend = function() {
-  var div = document.createElement("div");
-  div.appendChild(document.createTextNode("You put sunglasses on."));
-  eventArea.prepend(div);
-  imageArea.className = 'defend';
-}
+// Unicorn.prototype.defend = function() {
+//   var div = document.createElement("div");
+//   div.appendChild(document.createTextNode("You put sunglasses on."));
+//   eventArea.prepend(div);
+//   imageArea.className = 'defend';
+// }
 
 
 Unicorn.prototype.heal = function() {
