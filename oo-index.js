fd4910c5c18313231485226dff9e23cec9e2ccc5
Madison McCardle <madison.mccardle@powerhrg.com>
2020-07-21 15:20:26 -0400

    done

--- oo-index.js
+++ oo-index.js
@@ -1,3 +1,12 @@
+const eventArea = document.getElementById('event_area')
+const imageArea = document.getElementById('image_area')
+const restartButton = document.getElementById('restart').addEventListener("click",()=> Unicorn.prototype.restart())
+const attackButton = document.getElementById('attack').addEventListener("click",()=> Unicorn.prototype.attack())
+const defendButton = document.getElementById('defend').addEventListener("click",()=> Unicorn.prototype.defend())
+const healButton = document.getElementById('heal').addEventListener("click",()=> Unicorn.prototype.heal())
+const breakdanceButton = document.getElementById('breakdance').addEventListener("click",()=> Unicorn.prototype.breakdance())
+const eatButton = document.getElementById('eat').addEventListener("click",()=> Unicorn.prototype.eatCronut())
+
 class Unicorn {
   constructor(name, occupation) {
     this.name = name;
@@ -6,3 +15,56 @@ class Unicorn {
   }
 
 }
+
+
+Unicorn.prototype.restart = function() {
+  while (eventArea.hasChildNodes()) {
+    eventArea.removeChild(eventArea.lastChild);
+}
+var div = document.createElement("div");
+div.appendChild(document.createTextNode("Out on the street, ready to go!"));
+eventArea.prepend(div);
+imageArea.className = 'new';
+}
+
+
+Unicorn.prototype.attack = function() {
+  var div = document.createElement("div");
+  div.appendChild(document.createTextNode("'ATTAAAAAAAAAK!''"));
+  eventArea.prepend(div);
+  imageArea.className = 'attack';
+}
+
+
+Unicorn.prototype.defend = function() {
+  var div = document.createElement("div");
+  div.appendChild(document.createTextNode("You put sunglasses on."));
+  eventArea.prepend(div);
+  imageArea.className = 'defend';
+}
+
+
+Unicorn.prototype.heal = function() {
+  var div = document.createElement("div");
+  div.appendChild(document.createTextNode("You feel refreshed after yoga."));
+  eventArea.prepend(div);
+  imageArea.className = 'heal';
+}
+
+
+Unicorn.prototype.breakdance = function() {
+  var div = document.createElement("div");
+  div.appendChild(document.createTextNode("It's really spinning class"));
+  eventArea.prepend(div);
+  imageArea.className = 'breakdance';
+}
+
+
+
+Unicorn.prototype.eatCronut = function() {
+  var div = document.createElement("div");
+  div.appendChild(document.createTextNode("You've gained 1000 health points."));
+  eventArea.prepend(div);
+  imageArea.className = 'eat';
+}
+
